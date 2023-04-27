745f8a297b46fefb1b338d0b534158f3baa8be9f
brenofflank <107095857+Klackters@users.noreply.github.com>
2023-04-27 10:54:13 -0300

    up organize

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require("express");   //
-const app = express();
+navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    console.log("Latitude: " + latitude + " Longitude: " + longitude);
+  });
 
-app.use((express.json));
+$('#form').submit((e) => {
+  e.preventDefault()
 
+  var url = $('#url').val()
 
+  var number = $("#number").val()
+
+  getBlogPosts(url, number)
+})
+
+$("#number").change((e) => {
+  number = $(this).val()
+})
+
+function getBlogPosts(url, number) {
+  $("#data").html('')
+  $.ajax({
+    method: "GET",
+    url: `https://${url}/wp-json/wp/v2/posts?_fields[]=title&_fields[]=link&_fields[]=date&per_page=${number}`,
+    success:function(data) {
+      console.log(data)
+
+      data.forEach(post => {
+        $("#data").append(`
+        
+        <tr>
+          <td><a href="${post.link}" target="_blank">${post.title.rendered}</a></td>
+          <td>${post.date}</td>
+          </tr>
+
+        `)
+      });
+    }
+  })
+}
 
